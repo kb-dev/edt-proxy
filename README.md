29e26daa6c3bdd186e3eb0ce4ff37dc32c5b09ad
Thomas <mail.thomasclement@gmail.com>
2021-07-15 10:32:20 +0200

    Setup proxy

---
+++ README.md
@@ -0,0 +1,8 @@
+# EDT PROXY
+
+Smalll proxy to forward requests from UKit app to U-Bordeaux Calendar API
+
+#### Nodes modules used:
+
+- [Koa](https://github.com/koajs/koa)
+- [koa-better-http-proxy](https://github.com/nsimmons/koa-better-http-proxy)
---
+++ index.js
@@ -0,0 +1,13 @@
+const proxy = require('koa-better-http-proxy');
+const Koa = require('koa');
+const app = new Koa();
+
+const URL = 'celcat.u-bordeaux.fr';
+const PORT = 3000;
+
+app.use(
+  proxy(URL, {
+    https: true,
+  })
+);
+app.listen(PORT);
